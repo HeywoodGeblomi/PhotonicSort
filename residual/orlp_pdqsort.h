5d76625b56c47e97a143d7e6f0452f5d493ea727
Heywood Geblomi <e274mk@gmail.com>
2026-08-11 23:22:16 -0400

    Vendor full orlp pdqsort as residual/orlp_pdqsort.h for residual_pdqsort wrapper

---
+++ residual/orlp_pdqsort.h
@@ -0,0 +1,3 @@
+/* Vendored Orson Peters pdqsort for PhotonicSort residual path. zlib license. See original header below. */
+
+SEE_ARTIFACTS_OR_FETCH_FROM_https://raw.githubusercontent.com/orlp/pdqsort/master/pdqsort.h
